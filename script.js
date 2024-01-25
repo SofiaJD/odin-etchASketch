1789e7ca7c2f44a5fb4e8028f69365c1877cad08
Sofía J. D. <59852487+SofiaJD@users.noreply.github.com>
2024-01-24 22:35:15 -0400

    Add grid 16*16

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('#container');
+
+function createRow()
+{
+    const row = document.createElement('div');
+    row.className = 'row';
+    container.appendChild(row);
+
+    for(let i = 0; i < 16; i++)
+    {
+        let div = document.createElement('div');
+        div.className = 'square';
+        row.appendChild(div);
+    }
+
+}
+
+function createGrid()
+{
+    for(let i = 0; i < 16; i++)
+    {
+        createRow();
+    }
+}
+
+createGrid();
